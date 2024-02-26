6f622db95bf0175f95bf3105caa78671381ac4ff
komal-chavan9090 <155181393+komal-chavan9090@users.noreply.github.com>
2024-02-26 15:44:19 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Power Set/power-set.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+public:
+void help(vector<string>&ans,string s,string ch,int i){
+    if(i==s.length()){
+        if(ch!=""){
+        ans.push_back(ch);
+        }
+        return;
+        
+        }
+      help(ans,s,ch,i+1);
+     help(ans,s,ch+s[i],i+1);
+ 
+}
+vector<string> AllPossibleStrings(string s){
+   vector<string>ans;
+   help(ans,s,"",0);
+   sort (ans.begin(),ans.end());
+    return ans;
+    
+}
+};
+
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string s;
+		cin >> s;
+		Solution ob;
+		vector<string> res = ob.AllPossibleStrings(s);
+		for(auto i : res)
+			cout << i <<" ";
+		cout << "\n";
+
+	}
+	return 0;
+}
+// } Driver Code Ends
