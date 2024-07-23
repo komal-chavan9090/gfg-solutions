3b64fad279b83bfa48c63ede899ab8ae22213656
komal-chavan9090 <155181393+komal-chavan9090@users.noreply.github.com>
2024-07-23 11:56:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shortest Job first/shortest-job-first.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+// Back-end complete function Template for C++
+
+class Solution {
+  public:
+    long long solve(vector<int>& bt) {
+        sort(bt.begin(),bt.end());
+        // code here
+        long long startAt=0;
+        long long endsAt=bt[0];
+        
+        for(int i=1;i<bt.size();i++)
+        {
+            startAt+=endsAt;
+            endsAt+=bt[i];
+        }
+        return startAt/bt.size();
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> bt(n), p(n);
+        for (int i = 0; i < n; i++) {
+            cin >> bt[i];
+        }
+        Solution obj;
+        long long ans = obj.solve(bt);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
